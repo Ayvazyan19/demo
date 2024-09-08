f3384c9f2a3d37fbf67d60fb9e5424610053feaa
Ayvazyan19 <gorikayvazyan@gmail.com>
2024-09-08 21:01:31 +0400

    demo-project

---
+++ libs/js/script.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        900:{
+            items:4
+        },
+        1200:{
+            items:5
+        },
+    }
+})
